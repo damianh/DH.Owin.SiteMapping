3fdd0479e4af6453171ba89b74bf6d3ae7dfa4b8
Damian Hickey <dhickey@gmail.com>
2013-10-26 22:05:23 +0100

    Using MS.Owin.Testing

--- src/Owin.SiteMapping.Tests/SiteMapTests.cs
+++ src/Owin.SiteMapping.Tests/SiteMapTests.cs
@@ -5,21 +5,21 @@
     using System.Threading.Tasks;
     using FluentAssertions;
     using Microsoft.Owin.Infrastructure;
-    using Testing;
+    using Microsoft.Owin.Testing;
     using Xunit;
 
     public class SiteMapTests
     {
-        private readonly OwinTestServer _testServer;
+        private readonly TestServer _testServer;
 
         public SiteMapTests()
         {
-            _testServer = OwinTestServer.Create(
+            _testServer = TestServer.Create(
                 builder =>
                 {
                     SignatureConversions.AddConversions(builder); // supports Microsoft.Owin.OwinMiddleWare
                     builder.MapSite(new SiteMapConfig("example.com"),
-                        branch => branch.Use(context =>
+                        branch => branch.Use((context, _) =>
                         {
                             context.Response.StatusCode = 200;
                             context.Response.ReasonPhrase = "OK";
@@ -31,7 +31,7 @@
         [Fact]
         public async Task When_site_is_mapped_then_should_get_OK()
         {
-            HttpClient httpClient = _testServer.CreateHttpClient();
+            HttpClient httpClient = _testServer.HttpClient;
             HttpResponseMessage response = await httpClient.GetAsync("http://example.com");
             response.StatusCode.Should().Be(HttpStatusCode.OK);
         }
@@ -39,7 +39,7 @@
         [Fact]
         public async Task When_site_is_unknown_then_should_get_NotFound()
         {
-            HttpClient httpClient = _testServer.CreateHttpClient();
+            HttpClient httpClient = _testServer.HttpClient;
             HttpResponseMessage response = await httpClient.GetAsync("http://example.com");
             response.StatusCode.Should().Be(HttpStatusCode.OK);
         }
--- src/Owin.SiteMapping/SiteMapMiddleware.cs
+++ src/Owin.SiteMapping/SiteMapMiddleware.cs
@@ -1,47 +1,48 @@
 ﻿namespace Owin.SiteMapping
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Threading.Tasks;
-    using Microsoft.Owin;
+	using System;
+	using System.Collections.Generic;
+	using System.Threading.Tasks;
+	using Microsoft.Owin;
 
-    public class SiteMapMiddleware
-    {
-        private readonly Func<IDictionary<string, object>, Task> _next;
-        private readonly Func<IDictionary<string, object>, Task> _branch;
-        private readonly HashSet<SiteMapConfig> _siteMaps;
+	public class SiteMapMiddleware
+	{
+		private readonly Func<IDictionary<string, object>, Task> _next;
+		private readonly Func<IDictionary<string, object>, Task> _branch;
+		private readonly HashSet<SiteMapConfig> _siteMaps;
 
-        public SiteMapMiddleware(Func<IDictionary<string, object>, Task> next, Func<IDictionary<string, object>, Task> branch, IEnumerable<SiteMapConfig> siteMaps)
-        {
-            if (next == null)
-            {
-                throw new ArgumentNullException("next");
-            }
-            if (branch == null)
-            {
-                throw new ArgumentNullException("branch");
-            }
-            if (siteMaps == null)
-            {
-                throw new ArgumentNullException("siteMaps");
-            }
+		public SiteMapMiddleware(Func<IDictionary<string, object>, Task> next, Func<IDictionary<string, object>, Task> branch,
+		                         IEnumerable<SiteMapConfig> siteMaps)
+		{
+			if (next == null)
+			{
+				throw new ArgumentNullException("next");
+			}
+			if (branch == null)
+			{
+				throw new ArgumentNullException("branch");
+			}
+			if (siteMaps == null)
+			{
+				throw new ArgumentNullException("siteMaps");
+			}
 
-            _next = next;
-            _branch = branch;
-            _siteMaps = new HashSet<SiteMapConfig>(siteMaps);
-        }
+			_next = next;
+			_branch = branch;
+			_siteMaps = new HashSet<SiteMapConfig>(siteMaps);
+		}
 
-        public Task Invoke(IDictionary<string, object> environment)
-        {
-            if (environment == null)
-            {
-                throw new ArgumentNullException("environment");
-            }
-            var request = new OwinRequest(environment);
-            var requestScheme = (RequestScheme)Enum.Parse(typeof(RequestScheme), request.Scheme, true);
-            var port = request.LocalPort;
-            var siteMap = new SiteMapConfig(request.Host, requestScheme, port ?? 80);
-            return _siteMaps.Contains(siteMap) ? _branch(request.Environment) : _next(request.Environment);
-        }
-    }
+		public Task Invoke(IDictionary<string, object> environment)
+		{
+			if (environment == null)
+			{
+				throw new ArgumentNullException("environment");
+			}
+			var request = new OwinRequest(environment);
+			var requestScheme = (RequestScheme) Enum.Parse(typeof (RequestScheme), request.Scheme, true);
+			var port = request.LocalPort ?? 80;
+			var siteMap = new SiteMapConfig(request.Host.Value, requestScheme, port);
+			return _siteMaps.Contains(siteMap) ? _branch(request.Environment) : _next(request.Environment);
+		}
+	}
 }
