97526b919696de70a47d5f37e21091c7bbd8c1b8
Damian Hickey <dhickey@gmail.com>
2014-06-21 19:52:38 +0200

    Build script update and site mapping nuspec

--- default.ps1
+++ default.ps1
@@ -1,5 +1,5 @@
 properties { 
-	$projectName = "Owin.SiteMapping"
+	$projectName = "SiteMappingMiddleware"
 	$buildNumber = 0
 	$rootDir  = Resolve-Path .\
 	$buildOutputDir = "$rootDir\build"
@@ -9,7 +9,7 @@ properties {
 	$assemblyInfoFilePath = "$srcDir\SharedAssemblyInfo.cs"
 }
 
-task default -depends Clean, UpdateVersion, RunTests, ILRepack, CopyBuildOutput, CreateNuGetPackages
+task default -depends Clean, UpdateVersion, RunTests, CreateNuGetPackages
 
 task Clean {
 	Remove-Item $buildOutputDir -Force -Recurse -ErrorAction SilentlyContinue
@@ -29,7 +29,7 @@ task Compile {
 
 task RunTests -depends Compile {
 	$xunitRunner = "$srcDir\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe"
-	Get-ChildItem . -Recurse -Include *Tests.csproj, Tests.*.csproj | % {
+	gci . -Recurse -Include *Tests.csproj, Tests.*.csproj | % {
 		$project = $_.BaseName
 		if(!(Test-Path $reportsDir\xUnit\$project)){
 			New-Item $reportsDir\xUnit\$project -Type Directory
@@ -38,22 +38,11 @@ task RunTests -depends Compile {
     }
 }
 
-task ILRepack -depends Compile {
-	$ilrepack = "$srcDir\packages\ILRepack.1.25.0\tools\ILRepack.exe"
-	$workingDir = "$srcDir\Owin.SiteMapping\bin\Release"
-	.$ilrepack /targetplatform:v4 /internalize /target:library /out:$workingDir\$projectName.dll $workingDir\$projectName.dll $workingDir\Microsoft.Owin.dll $workingDir\Owin.dll
-}
-
-task CopyBuildOutput -depends Compile {
-	$binOutputDir = "$buildOutputDir\bin\$projectName\net40\"
-	New-Item $binOutputDir -Type Directory
-	gci $srcDir\$projectName\bin\Release |% { Copy-Item "$srcDir\$projectName\bin\Release\$_" $binOutputDir}
-}
-
-task CreateNuGetPackages -depends CopyBuildOutput {
+task CreateNuGetPackages -depends Compile {
 	$versionString = Get-Version $assemblyInfoFilePath
 	$version = New-Object Version $versionString
-	$packageVersion = $version.Major.ToString() + "." + $version.Minor.ToString() + "." + $version.Build.ToString()
-	copy-item $srcDir\$projectName\$projectName.nuspec $buildOutputDir
-	exec { .$srcDir\.nuget\nuget.exe pack $buildOutputDir\$projectName.nuspec -BasePath $buildOutputDir -o $buildOutputDir -version $packageVersion }
+	$packageVersion = $version.Major.ToString() + "." + $version.Minor.ToString() + "." + $version.Build.ToString() + "-build" + $buildNumber.ToString().PadLeft(5,'0')
+	gci $srcDir -Recurse -Include *.nuspec | % {
+		exec { .$srcDir\.nuget\nuget.exe pack $_ -o $buildOutputDir -version $packageVersion }
+	}
 }
